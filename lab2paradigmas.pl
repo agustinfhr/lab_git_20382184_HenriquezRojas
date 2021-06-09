199c306321d7e096e6eb2d5167103a4e90533b31
Agustin Henriquez <agustin.henriquez.r@usach.cl>
2021-06-08 22:38:31 -0400

    Representacion

---
+++ lab2paradigmas.pl
@@ -0,0 +1,21 @@
+
+
+
+ //////////////REPRESENTACION //////////////
+/*
+
+USUARIO => [nombre, contrasena, [id_post]]
+
+post => [post_id, contenido_post, autor_post, fecha_post]
+
+socialNetwork => [[Usuario1, Usuario2, ...], [Post1, Post2, ...], [Usuario_activo]]
+
+
+*/
+
+
+/*
+  Constructores 
+*/
+
+socialNetwork(Name, Date, OUTsocialNetwork).
